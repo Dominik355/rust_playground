a68b695b87c5726aceef389f800fcc726fb87c57
Dominik Bilik <dominik.bilik@firma.seznam.cz>
2024-10-11 08:35:27 +0200

    chore: removed generated data

--- arcswap-vs-leftright/benches/comparison.rs
+++ arcswap-vs-leftright/benches/comparison.rs
@@ -1,11 +1,34 @@
 use arcswap_vs_leftright::{
-    left_right_version, ArcSwapVersion, MutexVersion, RwLockVersion, ValueGetter,
+    ArcSwapVersion, LeftRightVersion, MutexVersion, RwLockVersion, ValueManipulator,
 };
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
-fn bench_only_writes<V: ValueGetter + 'static>(
+fn bench_with_few_writes<V: ValueManipulator + 'static>(
     getter: V,
-    num_iterations: usize,
+    num_iterations: u64,
+    parallelism: usize,
+) {
+    // have 1% of writes
+    let write_modulo = (num_iterations as f64 / 0.01) as u64;
+    std::thread::scope(|scope| {
+        for _ in 0..parallelism {
+            let getter = getter.clone();
+            scope.spawn(move || {
+                for i in 0..num_iterations {
+                    if i % write_modulo == 0 {
+                        black_box(getter.set_value(i));
+                    } else {
+                        black_box(getter.get_value());
+                    }
+                }
+            });
+        }
+    });
+}
+
+fn bench_only_reads<V: ValueManipulator + 'static>(
+    getter: V,
+    num_iterations: u64,
     parallelism: usize,
 ) {
     std::thread::scope(|scope| {
@@ -21,47 +44,83 @@ fn bench_only_writes<V: ValueGetter + 'static>(
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Atomic-reader");
+    let parallelisms = vec![1, 3, 10];
 
-    let iteration_counts = vec![10_000, 100_000, 500_000, 1_000_000, 5_000_000];
+    for parallelism in parallelisms {
+        let mut group = c.benchmark_group(format!("Atomic-reader-{}", parallelism));
 
-    for num_iterations in iteration_counts {
-        group.bench_with_input(
-            BenchmarkId::new("Mutex", num_iterations),
-            &num_iterations,
-            |b, &num_iterations| {
-                b.iter(|| bench_only_writes(MutexVersion::default(), num_iterations, 3));
-            },
-        );
+        let iteration_counts: Vec<u64> = vec![10_000, 100_000, 500_000, 1_000_000];
 
-        group.bench_with_input(
-            BenchmarkId::new("RwLock", num_iterations),
-            &num_iterations,
-            |b, &num_iterations| {
-                b.iter(|| bench_only_writes(RwLockVersion::default(), num_iterations, 3));
-            },
-        );
+        for num_iterations in iteration_counts {
+            group.bench_with_input(
+                BenchmarkId::new("Mutex-read", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_only_reads(MutexVersion::default(), num_iterations, 3));
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("ArcSwap", num_iterations),
-            &num_iterations,
-            |b, &num_iterations| {
-                b.iter(|| bench_only_writes(ArcSwapVersion::default(), num_iterations, 3));
-            },
-        );
+            group.bench_with_input(
+                BenchmarkId::new("RwLock-read", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_only_reads(RwLockVersion::default(), num_iterations, 3));
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("Left-Right", num_iterations),
-            &num_iterations,
-            |b, &num_iterations| {
-                b.iter(|| {
-                    let (_w, r) = left_right_version();
-                    bench_only_writes(r, num_iterations, 3)
-                });
-            },
-        );
+            group.bench_with_input(
+                BenchmarkId::new("ArcSwap-read", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_only_reads(ArcSwapVersion::default(), num_iterations, 3));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("Left-Right-read", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_only_reads(LeftRightVersion::default(), num_iterations, 3));
+                },
+            );
+
+            // read + 1% writes
+            group.bench_with_input(
+                BenchmarkId::new("Mutex-rw", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_with_few_writes(MutexVersion::default(), num_iterations, 3));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("RwLock-rw", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_with_few_writes(RwLockVersion::default(), num_iterations, 3));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("ArcSwap-rw", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| bench_with_few_writes(ArcSwapVersion::default(), num_iterations, 3));
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("Left-Right-rw", num_iterations),
+                &num_iterations,
+                |b, &num_iterations| {
+                    b.iter(|| {
+                        bench_with_few_writes(LeftRightVersion::default(), num_iterations, 3)
+                    });
+                },
+            );
+        }
+        group.finish();
     }
-    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
--- arcswap-vs-leftright/src/lib.rs
+++ arcswap-vs-leftright/src/lib.rs
@@ -4,77 +4,104 @@ use std::sync::{Arc, Mutex, RwLock};
 
 pub mod left_right_cell;
 
-pub trait ValueGetter: Clone + Send {
+pub trait ValueManipulator: Clone + Send {
     fn get_value(&self) -> u64;
+    fn set_value(&self, val: u64);
 }
 
 fn main() {}
 
-#[derive(Default, Clone)]
-pub struct InnerValue {
-    value: u64,
-}
-
-impl InnerValue {
-    fn get_value(&self) -> u64 {
-        self.value
-    }
-}
-
 // Arc Swap
 #[derive(Default, Clone)]
 pub struct ArcSwapVersion {
-    inner: Arc<ArcSwap<InnerValue>>,
+    inner: Arc<ArcSwap<u64>>,
 }
 
-impl ValueGetter for ArcSwapVersion {
+impl ValueManipulator for ArcSwapVersion {
     fn get_value(&self) -> u64 {
-        let inner = Access::<InnerValue>::load(&self.inner);
-        inner.get_value()
+        *Access::<u64>::load(&self.inner)
+    }
+
+    fn set_value(&self, val: u64) {
+        self.inner.store(Arc::new(val));
     }
 }
 
 // Left Right
 #[derive(Clone)]
 pub struct LeftRightVersion {
-    pub inner_w: Arc<Mutex<left_right_cell::WriteHandle<InnerValue>>>,
-    pub inner_r: left_right_cell::ReadHandle<InnerValue>,
+    pub inner_w: Arc<Mutex<left_right_cell::WriteHandle<u64>>>,
+    pub inner_r: left_right_cell::ReadHandle<u64>,
 }
 
-impl ValueGetter for left_right_cell::ReadHandle<InnerValue> {
+impl ValueManipulator for LeftRightVersion {
     fn get_value(&self) -> u64 {
-        self.get().unwrap().get_value()
+        *self.inner_r.get().unwrap()
+    }
+
+    fn set_value(&self, val: u64) {
+        let mut lock = self.inner_w.lock().unwrap();
+        lock.set(val);
+        lock.publish();
     }
 }
 
-pub fn left_right_version() -> (
-    Arc<Mutex<left_right_cell::WriteHandle<InnerValue>>>,
-    left_right_cell::ReadHandle<InnerValue>,
-) {
-    let (inner_w, inner_r) = left_right_cell::new_default::<InnerValue>();
-    (Arc::new(Mutex::new(inner_w)), inner_r)
+// impl ValueManipulator for left_right_cell::ReadHandle<u64> {
+//     fn get_value(&self) -> u64 {
+//         *self.get().unwrap()
+//     }
+//
+//     fn set_value(&self, val: u64) {
+//         todo!()
+//     }
+// }
+
+impl Default for LeftRightVersion {
+    fn default() -> Self {
+        let (inner_w, inner_r) = left_right_cell::new_default::<u64>();
+        Self {
+            inner_w: Arc::new(Mutex::new(inner_w)),
+            inner_r,
+        }
+    }
 }
+
+// pub fn left_right_version() -> (
+//     Arc<Mutex<left_right_cell::WriteHandle<u64>>>,
+//     left_right_cell::ReadHandle<u64>,
+// ) {
+//     let (inner_w, inner_r) = left_right_cell::new_default::<u64>();
+//     (Arc::new(Mutex::new(inner_w)), inner_r)
+// }
 
 // Mutex
 #[derive(Default, Clone)]
 pub struct MutexVersion {
-    inner: Arc<Mutex<InnerValue>>,
+    inner: Arc<Mutex<u64>>,
 }
 
-impl ValueGetter for MutexVersion {
+impl ValueManipulator for MutexVersion {
     fn get_value(&self) -> u64 {
-        self.inner.lock().unwrap().get_value()
+        *self.inner.lock().unwrap()
+    }
+
+    fn set_value(&self, val: u64) {
+        *self.inner.lock().unwrap() = val
     }
 }
 
 // RW Lock
 #[derive(Default, Clone)]
 pub struct RwLockVersion {
-    inner: Arc<RwLock<InnerValue>>,
+    inner: Arc<RwLock<u64>>,
 }
 
-impl ValueGetter for RwLockVersion {
+impl ValueManipulator for RwLockVersion {
     fn get_value(&self) -> u64 {
-        self.inner.read().unwrap().get_value()
+        *self.inner.read().unwrap()
+    }
+
+    fn set_value(&self, val: u64) {
+        *self.inner.write().unwrap() = val
     }
 }
